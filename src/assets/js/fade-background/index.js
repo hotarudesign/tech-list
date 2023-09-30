5db562d9a6d6dfd3c6ade0716b8a94cd36d154e7
hotarudesign <hotarudesign1220@gmail.com>
2023-09-30 11:53:24 +0900

    ✨ fade-backgroundの追加

---
+++ src/assets/js/fade-background/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+new Splide(".splide", {
+  autoplay: true,
+  type: "fade",
+  rewind: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  arrows: false,
+  pagination: false,
+  interval: 5000,
+  speed: 2000,
+}).mount();
